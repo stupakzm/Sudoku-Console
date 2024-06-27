4de58ec470a1e36955847839265ee48498ae6d05
Zakhar Stupak <85805156+stupakzm@users.noreply.github.com>
2024-06-27 20:11:54 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,347 @@
+﻿using Sudoku;
+using System.Diagnostics;
+
+class Program {
+    private static Stopwatch stopwatch = new Stopwatch();
+    private static int[,] board;// A board with which the user interacts
+    private static int[,] boardFull;// Generated solution
+    private static bool[,] boardSafePosition;// Starting positions
+    private static bool tookHint;
+    private static SudokuSequence hint;
+    private static int removedDigits = 0;
+    private const int GridSize = 9;
+    private static int[] indexColSelected = { 2, 4, 6, 10, 12, 14, 18, 20, 22 };// Need to visualize selected column
+
+    private static List<SudokuLog> log = new List<SudokuLog>();
+    private static Stack<SudokuSequence> sequence = new Stack<SudokuSequence>();
+
+    private static void Main(string[] args) {
+        tookHint = false;
+        removedDigits = 20;
+        Restart();
+    }
+
+    private static void PrintBoardMain() {
+        int col, row;
+        //need to move here after hint O
+        if (!tookHint) {
+            col = InputToInt("Input Column: ", 1, 9);
+            if (col == -1) return;
+            ClearConsole();
+            PrintBoard(board, ConsoleColor.White, gameAction: GameAction.InputRow, selectedCol: col - 1);
+            row = InputToInt("Input Row: ", 1, 9);
+            if (row == -1) return;
+            if (boardSafePosition[row - 1, col - 1]) {// Can`t change safe position
+                ClearConsole();
+                PrintBoard(board, ConsoleColor.Green, GameAction.InputColumn, col - 1, row - 1);
+                return;
+            }
+            ClearConsole();
+            PrintBoard(board, ConsoleColor.Red, 0, col - 1, row - 1);
+        }
+        else {
+            row = hint.positionRow + 1;
+            col = hint.positionCol + 1;
+        }
+        tookHint = false;
+        //need to move here after hint H
+        int numberToInsert = InputToInt("Input number to insert: ", 1, 9);
+        if (numberToInsert == -1) return;
+        bool condition = SudokuHandler.IsSafe(board, row - 1, col - 1, numberToInsert);//need to do this before adding number to board
+        int numberBefore = board[row - 1, col - 1];
+        sequence.Push(new SudokuSequence(row, col, numberBefore, numberToInsert));
+        if (board[row - 1, col - 1] == 0) {
+            log.Add(new SudokuLog(row, col, numberBefore, numberToInsert, Const.ACTION_INSERT));
+        }
+        else {
+            log.Add(new SudokuLog(row, col, numberBefore, numberToInsert, Const.ACTION_REPLACE));
+        }
+        board[row - 1, col - 1] = numberToInsert;
+
+        ClearConsole();
+        if (condition) {
+            PrintBoard(board, ConsoleColor.Blue, GameAction.InputColumn, col - 1, row - 1);
+        }
+        else {
+            PrintBoard(board, ConsoleColor.Yellow, GameAction.InputColumn, col - 1, row - 1);
+        }
+
+        CheckingIfBoardFull();
+    }
+
+    private static bool IsAllSafe() {
+        for (int i = 0; i < board.GetLength(0) - 1; i++) {
+            for (int j = 0; j < board.GetLength(1) - 1; j++) {
+                if (SudokuHandler.IsSafePlaced(board, i, j, board[i, j])) {
+                    //all good
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static void CheckingIfBoardFull()  {
+        if (IsBoardFull(board) && IsAllSafe()) {
+            string messageToRestart = "To start a new game write [r], to change the number of missing digits write [d]";
+            stopwatch.Stop();
+            ClearConsole();
+            PrintBoard(board, ConsoleColor.Gray);
+            Console.WriteLine($"Congratulations, you have completed the game in {stopwatch.Elapsed:mm\\:ss}, the number of missing digits was {removedDigits}.");
+            log.Add(new SudokuLog(0, 0, 0, 0, Const.ACTION_WON_GAME));
+            while (true) {
+                Console.WriteLine(messageToRestart);
+                var inputKey = Console.ReadKey();
+                if (inputKey.KeyChar == 'r') {
+                    Restart();
+                }
+                else if (inputKey.KeyChar == 'd') {
+                    int inputDigits = InputToInt("Input the number of missing digits u want [40-70]: ", 40, 70);
+                    removedDigits = inputDigits;
+                    Restart();
+                }
+                ClearConsole();
+            }
+        }
+    }
+
+    private static bool IsBoardFull(int[,] board) {
+        for (int i = 0; i < board.GetLength(0); i++) {
+            for (int j = 0; j < board.GetLength(1); j++) {
+                if (board[i, j] == 0) return false;
+            }
+        }
+        return true;
+    }
+
+    private static void Restart() {
+        boardFull = SudokuHandler.GenerateSudoku();
+        board = SudokuHandler.RemoveNumbers(boardFull, removedDigits);
+        AsignSafePositionsBoard();
+        Thread.Sleep(1000);
+        ClearConsole();
+        PrintBoard(board, ConsoleColor.Gray, GameAction.InputColumn);
+
+        stopwatch.Reset();
+        stopwatch.Start();
+
+        while (true) {
+            PrintBoardMain();
+        }
+    }
+
+    private static void AsignSafePositionsBoard() {
+        boardSafePosition = new bool[boardFull.GetLength(0), boardFull.GetLength(1)];
+        for (int i = 0; i < boardFull.GetLength(0); i++) {
+            for (int j = 0; j < boardFull.GetLength(1); j++) {
+                if (boardFull[i, j] == 0) boardSafePosition[i, j] = false;
+                else boardSafePosition[i, j] = true;
+            }
+        }
+    }
+
+    public static int InputToInt(string message, int fromIndex, int toIndex) {
+        int inputToReturn = 0;
+        bool isValidInput = false;
+
+        while (!isValidInput) {
+            Console.Write(message);
+            var inputString = Console.ReadKey();
+            if (toIndex > 10) {
+                var inputString2 = Console.ReadKey();
+                if (int.TryParse(inputString.KeyChar.ToString(), out inputToReturn)) {
+                    if (int.TryParse(inputString2.KeyChar.ToString(), out int inputToReturnConcat)) {
+                        int combinedInt = int.Parse(String.Concat(inputString.KeyChar.ToString(), inputString2.KeyChar.ToString()));
+                        if (combinedInt >= fromIndex && combinedInt <= toIndex) {
+                            return combinedInt;
+                        }
+                        continue;
+                    }
+                }
+                else {
+                    Console.WriteLine("Invalid input. Please enter a valid integer.");
+                }
+                continue;
+            }
+
+            if (inputString.Key == ConsoleKey.Backspace) {
+                if (sequence.TryPeek(out SudokuSequence previousMove)) {
+                    board[previousMove.positionRow - 1, previousMove.positionCol - 1] = previousMove.numberBefore;
+                    log.Add(new SudokuLog(previousMove.positionRow, previousMove.positionCol, previousMove.numberBefore, previousMove.numberAfter, Const.ACTION_DELETE));
+                    ClearConsole();
+                    PrintBoard(board, ConsoleColor.White, GameAction.InputColumn, previousMove.positionCol - 1, previousMove.positionRow - 1);
+                    sequence.Pop();
+                }
+                else {
+                    Console.WriteLine("Sequence is empty!");
+                }
+                continue;
+            }
+            else if (inputString.Key == ConsoleKey.Tab) {
+                ClearConsole();
+                for (int i = 0; i < log.Count; i++) {
+                    Console.WriteLine(i + log[i].ToString());
+                }
+                Console.ReadKey();
+                ClearConsole();
+                PrintBoard(board, ConsoleColor.White);
+                continue;
+            }
+            else if (inputString.Key == ConsoleKey.H) {// Position hint
+                hint = SudokuHandler.GetHint(board);
+                if (hint.numberAfter == 0) { Console.WriteLine("not found"); continue; }
+                tookHint = true;
+                ClearConsole();
+                PrintBoard(board, ConsoleColor.Red, 0, hint.positionCol, hint.positionRow);
+                return -1;
+            }
+            else if (inputString.Key == ConsoleKey.O) {// Opens number
+                hint = SudokuHandler.GetHint(board);
+                if (hint.numberAfter == 0) { Console.WriteLine("not found"); continue; }
+                sequence.Push(new SudokuSequence(hint.positionRow+1, hint.positionCol+1, hint.numberBefore, hint.numberAfter));
+                log.Add(new SudokuLog(hint.positionRow, hint.positionCol, hint.numberBefore, hint.numberAfter, Const.ACTION_HINT));
+
+                board[hint.positionRow, hint.positionCol] = hint.numberAfter;
+
+                ClearConsole();
+                PrintBoard(board, ConsoleColor.Blue, GameAction.InputColumn, hint.positionCol, hint.positionRow);
+                CheckingIfBoardFull();
+                return -1;
+            }
+
+            if (int.TryParse(inputString.KeyChar.ToString(), out inputToReturn)) {
+                if (inputToReturn >= fromIndex && inputToReturn <= toIndex) {
+                    isValidInput = true;
+                }
+                else {
+                    Console.WriteLine($"Please enter a number between {fromIndex} and {toIndex}.");
+                }
+            }
+            else {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+                Console.WriteLine("inputString is - " + inputString);
+            }
+        }
+
+        return inputToReturn;
+    }
+
+    public static void PrintBoard(int[,] board, ConsoleColor color, GameAction gameAction = 0, int selectedCol = -1, int selectedRow = -1) {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.Write("Inserted, ");
+        Console.ForegroundColor = ConsoleColor.Red;
+        //Console.Write("         Selected");
+        Console.Write("Selected, ");
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write("Warning, ");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write("Safe Position");
+        Console.ResetColor();
+        Console.WriteLine();
+        char[] line = AsignLine();
+
+        for (int i = 0; i < GridSize; i++) {
+            if (i % 3 == 0) {
+                if (i == 0 && gameAction == GameAction.InputColumn) {// Visualization before user input of the column
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.Write(line);
+                    Console.ResetColor();
+                    Console.WriteLine();
+                }
+                else if (i == 0 && selectedCol != -1) {// Visualization of the selected column
+                    for (int k = 0; k < line.Length; k++) {
+                        if (k == indexColSelected[selectedCol]) {
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            Console.Write(line[k]);
+                            Console.ResetColor();
+                        }
+                        else {
+                            Console.Write(line[k]);
+                        }
+                    }
+                    Console.WriteLine();
+                }
+                else {
+                    Console.WriteLine(line);
+                }
+            }
+            for (int j = 0; j < GridSize; j++) {
+                if (j % 3 == 0) {
+                    if (gameAction == GameAction.InputRow && j == 0) {// Visualization before user input of the row
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Write("| ");
+                        Console.ResetColor();
+                    }
+                    else if (selectedRow == i && j == 0 && gameAction != GameAction.InputColumn) {// Visualization of the selected row
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Write("| ");
+                        Console.ResetColor();
+                    }
+                    else {
+                        Console.Write("| ");
+                    }
+                }
+
+                string boardSymbol = board[i, j] == 0 ? "*" : board[i, j].ToString();
+                if (selectedRow == i && selectedCol == j) {// Print the selected position in color
+                    Console.ForegroundColor = color;
+                    Console.Write("{0} ", boardSymbol);
+                    Console.ResetColor();
+                    continue;
+                }
+
+                boardSymbol = board[i, j] == 0 ? "~" : board[i, j].ToString();
+                if (j == GridSize - 1) {
+                    Console.Write("{0} ", boardSymbol);
+                }
+                else {
+                    Console.Write("{0} ", boardSymbol);
+                }
+
+            }
+            Console.Write("|");
+            Console.WriteLine();
+        }
+        Console.WriteLine(line);
+        Console.WriteLine(FindAllFoundNumbers(board));
+    }
+
+    private static char[] AsignLine() {
+        int length = 25;
+        char[] result = new char[length];
+        for (int i = 0; i < length; i++) {
+            result[i] = '-';
+        }
+        return result;
+    }
+
+    private static string FindAllFoundNumbers(int[,] board) {
+        string stringToReturn = "Found numbers: ";
+        int[] numbers = new int[GridSize];
+        for (int i = 0; i < GridSize; i++) {
+            for (int j = 0; j < GridSize; j++) {
+                if (board[i, j] != 0) {
+                    numbers[board[i, j] - 1]++;
+                }
+            }
+        }
+        for (int i = 0; i < GridSize; i++) {
+            if (numbers[i] >= GridSize) {
+                //stringToReturn.Concat((i + 1) + " ");
+                stringToReturn += (i + 1 + " ").ToString();
+            }
+            else {
+                //stringToReturn.Concat("~ ");
+                stringToReturn += "~ ";
+            }
+        }
+        return stringToReturn;
+    }
+
+    private static void ClearConsole() {
+        Console.Clear();
+        Console.WriteLine($"Time elapsed: {stopwatch.Elapsed:mm\\:ss}");
+    }
+}
---
+++ SudokuHandler.cs
@@ -0,0 +1,242 @@
+﻿using Sudoku;
+
+public static class SudokuHandler {
+    private const int GridSize = 9;
+    
+    // Generate a Sudoku puzzle
+    public static int[,] GenerateSudoku() {
+        int[,] board = new int[GridSize, GridSize];
+        GenerateSudokuHelper(board);
+        return board;
+    }
+
+    // Helper function to generate Sudoku recursively
+    private static bool GenerateSudokuHelper(int[,] board) {
+        int row, col;
+
+        // Find an unassigned cell
+        if (!FindUnassignedCell(board, out row, out col)) {
+            return true; // If all cells are assigned, puzzle is complete
+        }
+
+        // Generate random permutation of numbers 1 to GridSize
+        int[] numbers = GenerateRandomPermutation();
+
+        // Try placing a number in the cell
+        foreach (int num in numbers) {
+            if (IsSafe(board, row, col, num)) {
+                board[row, col] = num;
+
+                // Recur to fill remaining cells
+                if (GenerateSudokuHelper(board)) {
+                    return true; // If solution found, return true
+                }
+
+                // If placing num at (row, col) doesn't lead to solution, backtrack
+                board[row, col] = 0;
+            }
+        }
+        // No number can be placed, return false to trigger backtracking
+        return false;
+    }
+
+    // Find an unassigned cell
+    private static bool FindUnassignedCell(int[,] board, out int row, out int col) {
+        for (row = 0; row < GridSize; row++) {
+            for (col = 0; col < GridSize; col++) {
+                if (board[row, col] == 0) {
+                    return true;
+                }
+            }
+        }
+        row = -1; col = -1; // If no unassigned cell found
+        return false;
+    }
+
+    // Check if it's safe to place num at (row, col)
+    public static bool IsSafe(int[,] board, int row, int col, int num) {
+        // Check row and column
+        for (int i = 0; i < GridSize; i++) {
+            if (board[row, i] == num || board[i, col] == num) {
+                return false;
+            }
+        }
+
+        // Check 3x3 square
+        int boxRow = row - row % 3;
+        int boxCol = col - col % 3;
+        for (int i = boxRow; i < boxRow + 3; i++) {
+            for (int j = boxCol; j < boxCol + 3; j++) {
+                if (board[i, j] == num) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    // Check if it's safe placed num at (row, col)
+    public static bool IsSafePlaced(int[,] board, int row, int col, int num) {
+        // Check row and column
+        for (int i = 0; i < GridSize; i++) {
+            if (board[row, i] == num && i == col) {
+                continue;
+            }
+            if (board[i, col] == num && i == row) {
+                continue;
+            }
+            if (board[row, i] == num || board[i, col] == num) {
+                return false;
+            }
+        }
+
+        // Check 3x3 square
+        int boxRow = row - row % 3;
+        int boxCol = col - col % 3;
+        for (int i = boxRow; i < boxRow + 3; i++) {
+            for (int j = boxCol; j < boxCol + 3; j++) {
+                if (i == row && j == col) {
+                    continue;
+                }
+                if (board[i, j] == num) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    // Generate a random permutation of numbers 1 to GridSize
+    private static int[] GenerateRandomPermutation() {
+        int[] numbers = new int[GridSize];
+        for (int i = 0; i < GridSize; i++) {
+            numbers[i] = i + 1;
+        }
+
+        Random rand = new Random();
+        for (int i = GridSize - 1; i > 0; i--) {
+            int j = rand.Next(i + 1);
+            int temp = numbers[i];
+            numbers[i] = numbers[j];
+            numbers[j] = temp;
+        }
+
+        return numbers;
+    }
+
+   
+
+    public static int[,] RemoveNumbers(int[,] board, int numberOfCellsToRemove) {// numberOfCellsToRemove [40-70]
+        Random random = new Random();
+        int cellsRemoved = 0;
+
+        while (cellsRemoved < numberOfCellsToRemove) {
+            int row = random.Next(GridSize);
+            int col = random.Next(GridSize);
+
+            // Skip if the cell is already empty
+            if (board[row, col] == 0) {
+                continue;
+            }
+
+            // Backup the current value in case we need to backtrack
+            int backup = board[row, col];
+            board[row, col] = 0; // Remove the number from the cell
+
+            // Check if the puzzle still has a unique solution
+            if (!HasUniqueSolution(board)) {
+                // If the puzzle doesn't have a unique solution, revert the change
+                board[row, col] = backup;
+            }
+            else {
+                cellsRemoved++;
+            }
+        }
+        return board;
+    }
+
+    private static bool HasUniqueSolution(int[,] board) {
+        int[,] tempBoard = (int[,])board.Clone(); // Clone the board to avoid modifying the original
+        return SolveSudoku(tempBoard);
+    }
+
+    public static bool SolveSudoku(int[,] board) {
+        int row = 0, col = 0;
+        bool isDone = true;
+
+        // Find unassigned cell
+        for (row = 0; row < GridSize; row++) {
+            for (col = 0; col < GridSize; col++) {
+                if (board[row, col] == 0) {
+                    isDone = false;
+                    break;
+                }
+            }
+            if (!isDone) {
+                break;
+            }
+        }
+
+        // No unassigned cells left
+        if (isDone) {
+            return true;
+        }
+
+        // Try every digit from 1 to 9
+        for (int num = 1; num <= GridSize; num++) {
+            if (IsSafe(board, row, col, num)) {
+                board[row, col] = num;
+                if (SolveSudoku(board)) {
+                    return true;
+                }
+                board[row, col] = 0; // backtrack
+            }
+        }
+
+        return false;
+    }
+
+    public static SudokuSequence GetHint(int[,] board) {
+        int row = 0, col = 0;
+        bool isDone = true;
+
+        // Find unassigned cell
+        for (row = 0; row < GridSize; row++) {
+            for (col = 0; col < GridSize; col++) {
+                if (board[row, col] == 0) {
+                    for (int num = 1; num <= GridSize; num++) {
+                        if (IsSafe(board, row, col, num)) {
+                            // Check 3x3 square
+                            int boxRow = row - row % 3;
+                            int boxCol = col - col % 3;
+                            bool definitely = true;
+                            for (int i = boxRow; i < boxRow + 3; i++) {
+                                for (int j = boxCol; j < boxCol + 3; j++) {
+                                    if ((i == row && j == col) || !definitely) {
+                                        continue;
+                                    }
+                                    if (board[i, j] == 0) {
+                                        // Check row and column
+                                        if (IsSafe(board, i, j, num)) {
+                                            definitely = false;
+                                        }
+                                    }
+                                }
+                            }
+                            if (definitely) {
+                                return new SudokuSequence(row, col, board[row, col], num);
+                            }
+                        }
+                    }
+                }
+            }
+            if (!isDone) {
+                break;
+            }
+        }
+        return new SudokuSequence();
+    }
+    
+}
